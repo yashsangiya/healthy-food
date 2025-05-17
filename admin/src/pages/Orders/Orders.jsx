9045cd9886b0c6ced76efd2898e5219572d45c71
yashsangiya <ysangiya@gmail.com>
2025-05-17 23:00:58 +0530

    Add files via upload

---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const Orders = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Orders
+
+/*import React from 'react'
+import './Orders.css'
+import { useState } from 'react'
+import {toast} from "react-toastify"
+import { useEffect } from 'react'
+import axios from "axios"
+import { assets } from "../../assets/assets"
+
+const Orders = ({url}) => {
+    const [orders,setOrders] = useState([]);
+    const fetchAllOrders = async ()=>{
+      const response = await axios.get(url+"/api/order/list");
+      if(response.data.success){
+        setOrders(response.data.data);
+        console.log(response.data.data);
+      }
+      else{
+        toast.error("Error")
+      }
+    }
+    useEffect(()=>{
+      fetchAllOrders();
+    },[])
+  return (
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className='order-list'>
+        {orders.map((order,index)=>(
+          <div key={index} className='order-item'>
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className='order-item-food'>
+                {order.items.map((item,index)=>{
+                  if(index===order.items.length-1){
+                    return item.name + " X " + item.quantity
+                  }
+                  else{
+                    return item.name+" X "+item.quantity+","
+                  }
+                })}
+              </p>
+              <p className="order-item-name">{order.address.firstName+" "+order.address.lastName}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Orders
+*/
